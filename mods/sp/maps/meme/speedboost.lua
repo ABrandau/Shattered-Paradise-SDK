54a07e9c3faf3597421393b5499de873d025a7cb
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2020-02-01 21:09:11 +0300

    Map folder cleanup.

--- mods/sp/maps/meme/speedboost.lua
+++
@@ -1,7 +0,0 @@
-WorldLoaded = function()
-	Utils.Do(Map.ActorsInWorld, function(actor)
-		if actor.HasProperty("GrantCondition") then
-			actor.GrantCondition("speedboost", DateTime.Seconds(30))
-		end
-	end)
-end
