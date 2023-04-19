a527351489ed63d3d7f0f5f67b100e7fc66fc14c
dnqbob <dnqbob@outlook.com>
2023-04-19 22:56:15 +0800

    Add SpawnSparks for damaged spark generation, which is cheaper on perf .

---
+++ OpenRA.Mods.Sp/Traits/SpawnSparks.cs
@@ -0,0 +1,162 @@
+﻿using System.Linq;
+using OpenRA.GameRules;
+using OpenRA.Mods.Common.Effects;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Sp.Traits
+{
+	[Desc("Support spark weapons spawning or just simply generates an effect after an interval.")]
+	class SpawnSparksInfo : ConditionalTraitInfo, IRulesetLoaded
+	{
+		[WeaponReference]
+		[Desc("Interval of each spark spawning.")]
+		public readonly int Interval = 100;
+
+		[Desc("Delay of first spark spawning.")]
+		public readonly int Delay = 0;
+
+		[Desc("Has to be defined in weapons.yaml as well.")]
+		public readonly string SparkWeapon = null;
+
+		[Desc("Amount of weapons fired.")]
+		public readonly int Amount = 0;
+
+		public readonly bool ForceToGround = true;
+
+		public readonly bool ResetReloadWhenEnabled = true;
+
+		public WeaponInfo WeaponInfo { get; private set; }
+
+		[Desc("Explosion offset relative to actor's position.")]
+		public readonly WVec LocalOffset = WVec.Zero;
+
+		[Desc("Image containing launch effect sequence.")]
+		public readonly string LaunchEffectImage = null;
+
+		[Desc("Launch effect sequence to play.")]
+		[SequenceReference(nameof(LaunchEffectImage), allowNullImage: true)]
+		public readonly string[] LaunchEffectSequences = null;
+
+		[Desc("Palette to use for launch effect.")]
+		[PaletteReference]
+		public readonly string LaunchEffectPalette = "effect";
+
+		public override object Create(ActorInitializer init) { return new SpawnSparks(this, init.Self); }
+
+		public override void RulesetLoaded(Ruleset rules, ActorInfo ai)
+		{
+			base.RulesetLoaded(rules, ai);
+
+			if (Amount <= 0)
+				return;
+
+			if (SparkWeapon == null)
+				throw new YamlException("When Amount > 0, Weapon must have a value");
+
+			var weaponToLower = SparkWeapon.ToLowerInvariant();
+			if (!rules.Weapons.TryGetValue(weaponToLower, out var weaponInfo))
+				throw new YamlException("Weapons Ruleset does not contain an entry '{0}'".F(weaponToLower));
+
+			WeaponInfo = weaponInfo;
+		}
+	}
+
+	class SpawnSparks: ConditionalTrait<SpawnSparksInfo>, ITick
+	{
+		readonly SpawnSparksInfo info;
+		readonly WeaponInfo weapon;
+		readonly BodyOrientation body;
+		bool hasWeapon;
+		bool hasLaunchEffect;
+
+		int interval;
+
+		public SpawnSparks(SpawnSparksInfo info, Actor self)
+			: base(info)
+		{
+			this.info = info;
+			hasWeapon = info.SparkWeapon != null && info.Amount > 0;
+			weapon = info.WeaponInfo;
+			body = self.TraitOrDefault<BodyOrientation>();
+			hasLaunchEffect = !string.IsNullOrEmpty(info.LaunchEffectImage) && info.LaunchEffectSequences?.Length > 0;
+			interval = info.Delay;
+		}
+
+		void ITick.Tick(Actor self)
+		{
+			if (IsTraitDisabled)
+				return;
+
+			if (--interval <= 0)
+			{
+				var epicenter = self.CenterPosition + (body != null
+					? body.LocalToWorld(info.LocalOffset.Rotate(body.QuantizeOrientation(self.Orientation)))
+					: info.LocalOffset);
+				var world = self.World;
+
+				if (hasLaunchEffect)
+				{
+					self.World.AddFrameEndTask(w => w.Add(new SpriteEffect(epicenter, self.World,
+						info.LaunchEffectImage, info.LaunchEffectSequences.Random(world.LocalRandom), info.LaunchEffectPalette)));
+				}
+
+				if (!hasWeapon)
+				{
+					interval = info.Interval;
+					return;
+				}
+
+				var map = world.Map;
+				var amount = info.Amount;
+				var offset = 1024 / amount;
+				for (var i = 0; i < amount; i++)
+				{
+					var rotation = WRot.FromYaw(new WAngle(i * offset));
+					var targetpos = epicenter + new WVec(weapon.Range.Length, 0, 0).Rotate(rotation);
+					var radiusTarget = Target.FromPos(new WPos(targetpos.X, targetpos.Y, info.ForceToGround ? map.CenterOfCell(map.CellContaining(targetpos)).Z : targetpos.Z));
+
+					var projectileArgs = new ProjectileArgs
+					{
+						Weapon = weapon,
+						Facing = default,
+						CurrentMuzzleFacing = () => default,
+
+						DamageModifiers = new int[] { 1 },
+
+						InaccuracyModifiers = new int[] { 1 },
+
+						RangeModifiers = new int[] { 1 },
+						Source = epicenter,
+						CurrentSource = () => epicenter,
+						SourceActor = self,
+						GuidedTarget = radiusTarget,
+						PassiveTarget = radiusTarget.CenterPosition
+					};
+
+					if (projectileArgs.Weapon.Projectile != null)
+					{
+						var projectile = projectileArgs.Weapon.Projectile.Create(projectileArgs);
+						if (projectile != null)
+							world.AddFrameEndTask(w => w.Add(projectile));
+					}
+				}
+
+				if (weapon.Report != null && weapon.Report.Any())
+				{
+					if (weapon.AudibleThroughFog || (!self.World.ShroudObscures(epicenter) && !self.World.FogObscures(epicenter)))
+						Game.Sound.Play(SoundType.World, weapon.Report, world, epicenter, null, weapon.SoundVolume);
+				}
+
+				interval = info.Interval;
+			}
+		}
+
+		protected override void TraitEnabled(Actor self)
+		{
+			if (info.ResetReloadWhenEnabled)
+				interval = 0;
+		}
+
+	}
+}
