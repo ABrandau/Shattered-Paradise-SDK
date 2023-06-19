fedf4077fa54bac0b2b49f6955e4fbcf05eaf885
dnqbob <dnqbob@outlook.com>
2023-06-19 22:57:43 +0800

    Solve game options conflict
    1. Solve no-base and sudden death
    2. Solve no-base and short game
    3. add a king when no-base start with 1 or 2. when king is kill, game is lost.

---
+++ OpenRA.Mods.Sp/Traits/GrantConditionWhenEnableShortGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Sp.Traits
+{
+	sealed class GrantConditionWhenEnableShortGameInfo : TraitInfo
+	{
+		[FieldLoader.Require]
+		[GrantedConditionReference]
+		[Desc("The condition to grant to self")]
+		public readonly string Condition = null;
+
+		public override object Create(ActorInitializer init) { return new GrantConditionWhenEnableShortGame(this); }
+	}
+
+	sealed class GrantConditionWhenEnableShortGame: INotifyCreated
+	{
+		GrantConditionWhenEnableShortGameInfo info;
+		int token;
+
+		public GrantConditionWhenEnableShortGame(GrantConditionWhenEnableShortGameInfo info)
+		{
+			this.info = info;
+			token = Actor.InvalidConditionToken;
+		}
+
+		public void Created(Actor self)
+		{
+			if (self.Owner.World.WorldActor.Trait<MapOptions>().ShortGame)
+			{
+				if (token == Actor.InvalidConditionToken)
+					token = self.GrantCondition(info.Condition);
+			}
+		}
+	}
+}
