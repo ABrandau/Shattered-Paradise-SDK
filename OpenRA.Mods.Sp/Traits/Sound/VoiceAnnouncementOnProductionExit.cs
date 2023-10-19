22147deed47336e62269ff8ec1946c43e23fc269
dnqbob <dnqbob@outlook.com>
2023-10-19 17:21:19 +0800

    Add VoiceAnnouncementOnProductionExit and get rid of yaml hack of prodoction anouncement

---
+++ OpenRA.Mods.Sp/Traits/Sound/VoiceAnnouncementOnProductionExit.cs
@@ -0,0 +1,59 @@
+#region Copyright & License Information
+/*
+ * Copyright (c) The OpenRA Developers and Contributors
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.SP.Traits.Sound
+{
+	[Desc("Plays a voice clip when the actor is built.",
+		"HACK: production building must have Exit to enable this")]
+	public class VoiceAnnouncementOnProductionExitInfo : TraitInfo
+	{
+		[VoiceReference]
+		[FieldLoader.Require]
+		[Desc("Voice to play.")]
+		public readonly string Voice = null;
+
+		[Desc("Player relationships who can hear this voice. When set to None, play to owner only.")]
+		public readonly PlayerRelationship ValidRelationships = PlayerRelationship.Ally | PlayerRelationship.Neutral | PlayerRelationship.Enemy;
+
+		public override object Create(ActorInitializer init) { return new VoiceAnnouncementOnProductionExit(init, this); }
+	}
+
+	public class VoiceAnnouncementOnProductionExit : INotifyCreated
+	{
+		readonly bool hasNoCreationActivityDelay;
+		readonly VoiceAnnouncementOnProductionExitInfo info;
+
+		public VoiceAnnouncementOnProductionExit(ActorInitializer init, VoiceAnnouncementOnProductionExitInfo info)
+		{
+			hasNoCreationActivityDelay = init.GetOrDefault<CreationActivityDelayInit>() == null;
+			this.info = info;
+		}
+
+		public void Created(Actor self)
+		{
+			if (hasNoCreationActivityDelay)
+				return;
+
+			var player = self.World.LocalPlayer ?? self.World.RenderPlayer;
+			if (player == null)
+				return;
+
+			if (info.ValidRelationships.HasRelationship(self.Owner.RelationshipWith(player)))
+				self.PlayVoice(info.Voice);
+			else if (info.ValidRelationships == PlayerRelationship.None && self.Owner == player)
+				self.PlayVoice(info.Voice);
+		}
+	}
+}
