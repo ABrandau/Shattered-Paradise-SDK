d5e8f34bbed2d0dc3f9d22e994e4fba0d3806f3e
dnqbob <dnqbob@outlook.com>
2023-11-23 20:13:59 +0800

    Add UnpackBaseBotModule and McvManagerSPBotModule to solve problem of AI base cannot expand in a small place

---
+++ OpenRA.Mods.Sp/Traits/BotModules/UnpackBaseBotModule.cs
@@ -0,0 +1,106 @@
+#region Copyright & License Information
+/*
+ * Copyright (c) The OpenHV Developers (see https://github.com/OpenHV/OpenHV)
+ * This file is part of OpenHV, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.SP.Traits
+{
+	[Desc("Make AI move their expansion unit, when there is no more room for placing a new Mcv Factory, or we have plenty of MCV for normal production.")]
+	public class UnpackBaseBotModuleInfo : ConditionalTraitInfo
+	{
+		[Desc("Actor types that provides build radius.")]
+		public readonly HashSet<string> DeployedExpandVehicleTypes = new();
+
+		[Desc("Actor types that are able to produce MCVs.")]
+		public readonly HashSet<string> McvFactoryTypes = new();
+
+		[Desc("Actor types that are able to produce MCVs.")]
+		public readonly int MinDeployedExpandVehicleRequired = 3;
+
+		[Desc("Delay (in ticks) between unpacking unit at first, if there is no actor in McvFactoryTypes.")]
+		public readonly int InitialUndeployTick = 8500;
+
+		[Desc("Delay (in ticks) between unpacking unit.")]
+		public readonly int UndeployTick = 10000;
+
+		public override object Create(ActorInitializer init) { return new UnpackBaseBotModule(init.Self, this); }
+	}
+
+	public class UnpackBaseBotModule : ConditionalTrait<UnpackBaseBotModuleInfo>, IBotTick, IGameSaveTraitData
+	{
+		int undeployInterval;
+		bool initialUnpacked;
+		readonly World world;
+		readonly Player player;
+
+		readonly Predicate<Actor> validUnit;
+
+		public UnpackBaseBotModule(Actor self, UnpackBaseBotModuleInfo info)
+			: base(info)
+		{
+			world = self.World;
+			player = self.Owner;
+			undeployInterval = info.InitialUndeployTick;
+			validUnit = a => a != null && a.Owner == player && !a.IsDead && a.IsInWorld;
+		}
+
+		void IBotTick.BotTick(IBot bot)
+		{
+			if (--undeployInterval <= 0)
+			{
+				undeployInterval = Info.UndeployTick;
+				if (!initialUnpacked)
+				{
+					if (world.ActorsHavingTrait<Production>().Where(a => validUnit(a) && Info.McvFactoryTypes.Contains(a.Info.Name)).Any())
+						initialUnpacked = true;
+					else
+					{
+						var expand = world.ActorsHavingTrait<Transforms>().Where(a => validUnit(a) && Info.DeployedExpandVehicleTypes.Contains(a.Info.Name)).RandomOrDefault(world.LocalRandom);
+						if (expand != null)
+							bot.QueueOrder(new Order("DeployTransform", expand, true));
+					}
+				}
+				else
+				{
+					var expands = world.ActorsHavingTrait<Transforms>().Where(a => validUnit(a) && Info.DeployedExpandVehicleTypes.Contains(a.Info.Name)).ToList();
+					if (expands.Count >= Info.MinDeployedExpandVehicleRequired)
+						bot.QueueOrder(new Order("DeployTransform", expands.RandomOrDefault(world.LocalRandom), true));
+				}
+			}
+		}
+
+		List<MiniYamlNode> IGameSaveTraitData.IssueTraitData(Actor self)
+		{
+			if (IsTraitDisabled)
+				return null;
+
+			return new List<MiniYamlNode>()
+			{
+				new MiniYamlNode("UndeployInterval", FieldSaver.FormatValue(undeployInterval))
+			};
+		}
+
+		void IGameSaveTraitData.ResolveTraitData(Actor self, MiniYaml data)
+		{
+			if (self.World.IsReplay)
+				return;
+
+			var nodes = data.ToDictionary();
+
+			if (nodes.TryGetValue("UndeplyTick", out var initialBaseCenterNode))
+				undeployInterval = FieldLoader.GetValue<int>("UndeployInterval", initialBaseCenterNode.Value);
+		}
+	}
+}
