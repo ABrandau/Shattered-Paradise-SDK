49ee187f7e0f2c0ce8961fb9de7832f60db0282b
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2024-04-27 16:38:49 +0300

    Update lua copyright notices and fix GDI2 encoding.

--- mods/sp/maps/shellmap/luascript.lua
+++ mods/sp/maps/shellmap/luascript.lua
@@ -1,3 +1,12 @@
+--[[
+   Copyright (c) The OpenRA Developers and Contributors
+   This file is part of OpenRA, which is free software. It is made
+   available to you under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version. For more
+   information, see COPYING.
+]]
+
 GDIAttackForce = { "gdie1", "gdie1", "gdie1", "gdie1", "grenadier", "grenadier", "grenadier", "e2","mmch", "mmch", "apc", "smech"}
 GDIAirForce = { "orca", "orca", "orca" }
 NodTemplarRush = { "templar", "templar", "templar", "templar", "templar" }
